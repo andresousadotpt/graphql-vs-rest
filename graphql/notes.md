b31fb5b2b62cf17075567d6391d67493d7c740f0
André Sousa <andre.sousa@andresousa.pt>
2022-05-11 16:13:17 +0200

    Web server working

---
+++ graphql/notes.md
@@ -0,0 +1,6 @@
+So to get the intellisense to work I needed to install:
+
+> @types/express
+> @types/node
+
+If I didn't installed that packages I couldn't get the intellisense on node.js express to work
--- graphql/src/index.js
+++ graphql/src/index.js
@@ -1,2 +1,16 @@
+require("dotenv").config();
 const express = require("express");
+
 const app = express();
+const PORT =
+  process.env.ENVIRONMENT == "production"
+    ? process.env.PROD_PORT
+    : process.env.DEV_PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening to localhost:${PORT}`);
+});
